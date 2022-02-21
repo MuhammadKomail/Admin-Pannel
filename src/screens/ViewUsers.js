1f77b5b80ebf8fb83598f4301364c7226982d10e
MuhammadKomail <92737744+MuhammadKomail@users.noreply.github.com>
2022-02-21 11:36:24 +0500

    Fifth Commit

--- src/screens/ViewUsers.js
+++ src/screens/ViewUsers.js
@@ -1,55 +1,64 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { Container, Typography,Grid } from '@mui/material';
+import { Container, Typography, Grid } from '@mui/material';
+import axios from 'axios';
+
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
+  // { field: 'id', headerName: 'ID', width: 70 },
+  { field: '_id', headerName: 'id', width: 50 },
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    field: 'email',
+    headerName: 'Email',
+    width: 200,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+    field: 'password',
+    headerName: 'Password',
+    width: 150,
+  }
+  // {
+  //   field: 'fullName',
+  //   headerName: 'Full name',
+  //   description: 'This column has a value getter and is not sortable.',
+  //   sortable: false,
+  //   width: 190,
+  //   valueGetter: (params) =>
+  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  // },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
 
 export default function ViewUsers() {
+  const [userData, setUserData] = React.useState()
+
+  axios.get('http://localhost:5000/')
+    .then(res => setUserData(res.data))
+
+
+
+    const rows =  (!userData ? [{id:"Loading",firstName:"Loading",lastName:"Loading",email:"Loading",password:"Loading"}] : userData)
+  // [
+    // { id: 1, lastName: userData == null ? 'Loading...' : (userData[counter].lastName), firstName: userData == null ? 'Loading...' : (userData[counter].firstName), Email: userData == null ? 'Loading...' : (userData[counter].email) },
+  // ];
+
   return (
     <Container>
-    <Grid m={2}>
-    <Typography my={2} style={{ color: "gray", fontSize: 28 }}>Users</Typography>
-    </Grid>
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
+      <Grid m={2}>
+        <Typography my={2} style={{ color: "gray", fontSize: 28 }}>Users</Typography>
+      </Grid>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
     </Container>
   );
 }
